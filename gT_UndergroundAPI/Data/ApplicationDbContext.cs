b7a3ce424163aac323f9003b00622efc4af7ff83
Nick Poole <42500386+poolen1@users.noreply.github.com>
2023-08-09 17:51:08 -0700

    database connection/login works

---
+++ gT_UndergroundAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using gT_UndergroundAPI.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace gT_UndergroundAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base() 
+        { 
+        }
+
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options) 
+        { 
+        }
+        
+    }
+}
--- gT_UndergroundAPI/Program.cs
+++ gT_UndergroundAPI/Program.cs
@@ -1,4 +1,12 @@
 global using HealthCheckAPI;
+global using gT_UndergroundAPI;
+
+using gT_UndergroundAPI.Data;
+using gT_UndergroundAPI.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +25,48 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add ApplicationDbContext and SQL Server support
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+);
+
+// Add ASP.NET Core Identity support
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<JwtHandler>();
+
+// Add Auth services and middlewares
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        RequireExpirationTime = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.
+            GetBytes(builder.Configuration["JwtSettings:SecurityKey"]))
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +78,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseHealthChecks(new PathString("/api/health"),
